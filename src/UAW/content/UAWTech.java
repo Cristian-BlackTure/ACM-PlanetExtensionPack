6754b46ab22e303abcfef116262a42d8b678d11b
Cristian-BlackTure <110348555+Cristian-BlackTure@users.noreply.github.com>
2022-08-27 21:01:21 +0300

    Add files via upload

---
+++ src/UAW/content/UAWTech.java
@@ -0,0 +1,25 @@
+package UAW.content;
+
+import arc.struct.Seq;
+import mindustry.content.TechTree;
+import mindustry.content.TechTree.TechNode;
+import mindustry.ctype.ContentList;
+import mindustry.ctype.UnlockableContent;
+import mindustry.type.ItemStack;
+import mindustry.content.*;
+import mindustry.game.Objectives.*;
+
+import static mindustry.content.Blocks.*;
+import static mindustry.type.ItemStack.with;
+
+import static UAW.content.blocks.UAWBlocksDefense*;
+
+public static class UAWTechTree{
+    private static TechNode context = null;
+
+    public static void load(){
+        
+               attachNode(lancer, () -> {
+        	node(plasmic);
+    }
+    }
