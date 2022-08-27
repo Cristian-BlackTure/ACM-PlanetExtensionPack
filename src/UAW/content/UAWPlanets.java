758288f64f9dd299fea86718e1e3a5ae50cbe903
Cristian-BlackTure <110348555+Cristian-BlackTure@users.noreply.github.com>
2022-08-27 19:25:48 +0300

    Update UAWPlanets.java

--- src/UAW/content/UAWPlanets.java
+++ src/UAW/content/UAWPlanets.java
@@ -1,22 +1,57 @@
 package UAW.content;
 
+import arc.func.*;
 import arc.graphics.*;
-import mindustry.ctype.*;
-import mindustry.content.*;
+import arc.math.*;
+import arc.math.geom.*;
+import arc.struct.*;
+import arc.util.*;
+import mindustry.game.*;
+import mindustry.graphics.*;
 import mindustry.graphics.g3d.*;
+import mindustry.graphics.g3d.PlanetGrid.*;
 import mindustry.maps.planet.*;
 import mindustry.type.*;
-import UAW.planets.*;
+import mindustry.world.*;
+import mindustry.world.meta.*;
+import mindustry.content.*;
 
 public class UAWPlanets{
-	public static Planet vekt;
-    
-	public static void load() {
-		vekt = new Planet("vekt", Planets.sun, 3, 1) {{
-			generator = new GalemitePlanetGenerator();
-			meshLoader = () -> new HexMesh(this, 6);
-			startSector = 30;
-			alwaysUnlocked = true;
-		}};
-	}
-}
+    public static Planet
+    vekt;
+
+    public static void load(){
+               vekt = new Planet("vekt", Planets.sun, 1f, 3){{
+            generator = new SerpuloPlanetGenerator();
+            meshLoader = () -> new HexMesh(this, 6);
+            cloudMeshLoader = () -> new MultiMesh(
+                new HexSkyMesh(this, 11, 0.15f, 0.13f, 5, new Color().set(Pal.spore).mul(0.9f).a(0.75f), 2, 0.45f, 0.9f, 0.38f),
+                new HexSkyMesh(this, 1, 0.6f, 0.16f, 5, Color.white.cpy().lerp(Pal.spore, 0.55f).a(0.75f), 2, 0.45f, 1f, 0.41f)
+            );
+
+            launchCapacityMultiplier = 0.5f;
+            sectorSeed = 2;
+            allowWaves = true;
+            allowWaveSimulation = true;
+            allowSectorInvasion = true;
+            allowLaunchSchematics = true;
+            enemyCoreSpawnReplace = true;
+            allowLaunchLoadout = true;
+            //doesn't play well with configs
+            prebuildBase = false;
+            ruleSetter = r -> {
+                r.waveTeam = Team.crux;
+                r.placeRangeCheck = false;
+                r.attributes.clear();
+                r.showSpawns = false;
+            };
+            atmosphereColor = Color.valueOf("3c1b8f");
+            atmosphereRadIn = 0.02f;
+            atmosphereRadOut = 0.3f;
+            startSector = 15;
+            alwaysUnlocked = true;
+            landCloudColor = Pal.spore.cpy().a(0.5f);
+            hiddenItems.addAll(Items.erekirItems).removeAll(Items.serpuloItems);
+        }};
+       
+    }
