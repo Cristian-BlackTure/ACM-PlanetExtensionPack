eae0d97b4a6d2adcf9a531bc9ff17f9a21a73773
Cristian-BlackTure <110348555+Cristian-BlackTure@users.noreply.github.com>
2022-08-27 18:57:40 +0300

    Add files via upload

--- src/UAW/content/UAWPlanets.java
+++ src/UAW/content/UAWPlanets.java
@@ -6,13 +6,14 @@ import mindustry.content.*;
 import mindustry.graphics.g3d.*;
 import mindustry.maps.planet.*;
 import mindustry.type.*;
+import UAW.planets.*;
 
 public class UAWPlanets{
 	public static Planet vekt;
-
+    
 	public static void load() {
 		vekt = new Planet("vekt", Planets.sun, 3, 1) {{
-			generator = new ErekirPlanetGenerator();
+			generator = new GalemitePlanetGenerator();
 			meshLoader = () -> new HexMesh(this, 6);
 			atmosphereColor = Color.valueOf("3c1b8f");
 			atmosphereRadIn = 0.02f;
