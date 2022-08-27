ec7470a50e836143647518542facae26dd8bc52b
Cristian-BlackTure <110348555+Cristian-BlackTure@users.noreply.github.com>
2022-08-27 14:25:30 +0300

    Add files via upload

---
+++ src/UAW/content/UAWPlanets.java
@@ -0,0 +1,26 @@
+package UAW.content; 
+import java.net.Proxy.Type;
+  
+ import arc.graphics.*; 
+ import mindustry.ctype.*; 
+ import mindustry.content.*; 
+ import mindustry.graphics.g3d.*; 
+ import mindustry.maps.planet.*; 
+ import mindustryType.*; 
+  
+ public class UAWPlanets implements ContentList { 
+         public static Planet planetbl; 
+  
+         @Override 
+         public void load() { 
+                 planetbl = new Planet("planetbl", Planets.sun, 3, 1) {{ 
+                         generator = new GalemitePlanetGenerator(); 
+                         meshLoader = () -> new HexMesh(this, 6); 
+                         atmosphereColor = Color.valueOf("3c1b8f"); 
+                         atmosphereRadIn = 0.02f; 
+                         atmosphereRadOut = 0.3f; 
+                         startSector = 15; 
+                         alwaysUnlocked = true; 
+                 }}; 
+         } 
+ }
