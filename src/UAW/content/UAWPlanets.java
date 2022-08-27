1688103afee48798618537fe660b652616555f4c
Cristian-BlackTure <110348555+Cristian-BlackTure@users.noreply.github.com>
2022-08-27 11:55:04 +0300

    Add files via upload

---
+++ src/UAW/content/UAWPlanets.java
@@ -0,0 +1,84 @@
+package mindustry.content;
+
+import arc.func.*;
+import arc.graphics.*;
+import arc.math.*;
+import arc.math.geom.*;
+import arc.struct.*;
+import arc.util.*;
+import mindustry.game.*;
+import mindustry.graphics.*;
+import mindustry.graphics.g3d.*;
+import mindustry.graphics.g3d.PlanetGrid.*;
+import mindustry.maps.planet.*;
+import mindustry.type.*;
+import mindustry.world.*;
+import mindustry.world.meta.*;
+
+public class Planets{
+    public static Planet
+    sun,
+    planetbl;
+
+    public static void load(){
+        sun = new Planet("sun", null, 4f){{
+            bloom = true;
+            accessible = false;
+
+            meshLoader = () -> new SunMesh(
+                this, 4,
+                5, 0.3, 1.7, 1.2, 1,
+                1.1f,
+                Color.valueOf("ff7a38"),
+                Color.valueOf("ff9638"),
+                Color.valueOf("ffc64c"),
+                Color.valueOf("ffc64c"),
+                Color.valueOf("ffe371"),
+                Color.valueOf("f4ee8e")
+            );
+        }};
+
+       planetbl = new Planet("planetbl", sun, 1f, 2){{
+            generator = new ErekirPlanetGenerator();
+            meshLoader = () -> new HexMesh(this, 5);
+            cloudMeshLoader = () -> new MultiMesh(
+                new HexSkyMesh(this, 2, 0.15f, 0.14f, 5, Color.valueOf("eba768").a(0.75f), 2, 0.42f, 1f, 0.43f),
+                new HexSkyMesh(this, 3, 0.6f, 0.15f, 5, Color.valueOf("eea293").a(0.75f), 2, 0.42f, 1.2f, 0.45f)
+            );
+            alwaysUnlocked = true;
+            landCloudColor = Color.valueOf("ed6542");
+            atmosphereColor = Color.valueOf("f07218");
+            defaultEnv = Env.scorching | Env.terrestrial;
+            startSector = 10;
+            atmosphereRadIn = 0.02f;
+            atmosphereRadOut = 0.3f;
+            tidalLock = true;
+            orbitSpacing = 2f;
+            totalRadius += 2.6f;
+            lightSrcTo = 0.5f;
+            lightDstFrom = 0.2f;
+            clearSectorOnLose = true;
+            defaultCore = Blocks.coreBastion;
+            hiddenItems.addAll(Items.serpuloItems).removeAll(Items.erekirItems);
+
+            //TODO SHOULD there be lighting?
+            updateLighting = false;
+
+            ruleSetter = r -> {
+                r.waveTeam = Team.malis;
+                r.placeRangeCheck = false; //TODO true or false?
+                r.attributes.set(Attribute.heat, 0.8f);
+                r.showSpawns = true;
+                r.fog = true;
+                r.staticFog = true;
+                r.lighting = false;
+                r.coreDestroyClear = true;
+                r.onlyDepositCore = false; //TODO not sure
+            };
+
+            unlockedOnLand.add(Blocks.coreBastion);
+        }};
+        
+    }
+
+}
