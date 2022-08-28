475b41a1b30f6f37d58020ee21a9e831b0303509
Cristian-BlackTure <110348555+Cristian-BlackTure@users.noreply.github.com>
2022-08-28 21:07:38 +0300

    Update UAWPlanets.java

--- src/UAW/content/UAWPlanets.java
+++ src/UAW/content/UAWPlanets.java
@@ -15,7 +15,7 @@ import mindustry.type.*;
 import mindustry.world.*;
 import mindustry.world.meta.*;
 import mindustry.content.*;
-import mindustry.content.blocks;
+import mindustry.content.Blocks;
 import UAW.planets.*;
 
 public class UAWPlanets{
