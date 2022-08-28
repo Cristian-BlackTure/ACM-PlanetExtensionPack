288718cb7c3033866ff354e8c08dd16e2aec2408
Cristian-BlackTure <110348555+Cristian-BlackTure@users.noreply.github.com>
2022-08-29 01:12:43 +0300

    Update UAWPlanets.java

--- src/UAW/content/UAWPlanets.java
+++ src/UAW/content/UAWPlanets.java
@@ -53,7 +53,7 @@ public class UAWPlanets{
             startSector = 69;
             alwaysUnlocked = true;
             landCloudColor = Pal.spore.cpy().a(0.5f);
-            hiddenItems.addAll(Items.serpuloItems).removeAll(Items.erekirItems);
+            hiddenItems.addAll(Items.erekirItems).removeAll(Items.serpuloItems);
         }};
     }
 }
