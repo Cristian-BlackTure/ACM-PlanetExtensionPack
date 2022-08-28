5c52ab481017487b9214ed8625672cab36172bdb
Cristian-BlackTure <110348555+Cristian-BlackTure@users.noreply.github.com>
2022-08-28 08:41:48 +0300

    Add files via upload

---
+++ src/UAW/content/blocks/UAWEnv.java
@@ -0,0 +1,14 @@
+package UAW.content.blocks;
+
+import mindustry.world.blocks.environment.*;
+import mindustry.ctype.ContentList;
+import UAW.content.blocks*;
+
+public class UAWEnv{
+	public static Floor  h2d2;
+
+	public static void load() {
+
+		h2d2 = new Floor("h2d2") {{}};
+	}
+}
