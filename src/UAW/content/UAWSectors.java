c697d390a35392e3ca1bf1672bea16c68841c989
Cristian-BlackTure <110348555+Cristian-BlackTure@users.noreply.github.com>
2022-08-28 22:32:31 +0300

    Add files via upload

---
+++ src/UAW/content/UAWSectors.java
@@ -0,0 +1,24 @@
+package UAW.content;
+
+import mindustry.ctype.*;
+import mindustry.type.*;
+
+import UAW.planets.*;
+import mindustry.content.Planets.*;
+import UAW.maps.*;
+
+public static class UAWSectors{
+    public static SectorPreset
+    trynaStart;
+
+    @Override
+    public static void load(){
+
+        trynaStart = new SectorPreset("trynaStart", tryna, 69){{
+            alwaysUnlocked = true;
+            addStartingItems = true;
+            captureWave = 15;
+            difficulty = 1;
+        }};
+    }
+}
