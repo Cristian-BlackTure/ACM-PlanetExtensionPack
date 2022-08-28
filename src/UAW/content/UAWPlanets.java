c344742956c6eccec989b548c62f8b80b67d4375
Cristian-BlackTure <110348555+Cristian-BlackTure@users.noreply.github.com>
2022-08-28 11:15:53 +0300

    Update UAWPlanets.java

--- src/UAW/content/UAWPlanets.java
+++ src/UAW/content/UAWPlanets.java
@@ -15,6 +15,7 @@ import mindustry.type.*;
 import mindustry.world.*;
 import mindustry.world.meta.*;
 import mindustry.content.*;
+import UAW.planets.*;
 
 public class UAWPlanets{
     public static Planet
@@ -22,7 +23,7 @@ public class UAWPlanets{
 
     public static void load(){
                vekt = new Planet("vekt", Planets.sun, 1f, 3){{
-            generator = new SerpuloPlanetGenerator();
+            generator = new GalemitePlanetGenerator();
             meshLoader = () -> new HexMesh(this, 6);
             cloudMeshLoader = () -> new MultiMesh(
                 new HexSkyMesh(this, 11, 0.15f, 0.13f, 5, new Color().set(Pal.spore).mul(0.9f).a(0.75f), 2, 0.45f, 0.9f, 0.38f),
