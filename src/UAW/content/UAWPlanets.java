d946f5cda2ffdc391752865fee93f2d2336a0bbe
Cristian-BlackTure <110348555+Cristian-BlackTure@users.noreply.github.com>
2022-08-29 00:51:56 +0300

    Update UAWPlanets.java

--- src/UAW/content/UAWPlanets.java
+++ src/UAW/content/UAWPlanets.java
@@ -24,7 +24,7 @@ public class UAWPlanets{
 
     public static void load(){
                tyra = new Planet("tyra", Planets.sun, 1f, 3){{
-            generator = new GalemitePlanetGenerator();
+            generator = new TyraPlanetGenerator();
             meshLoader = () -> new HexMesh(this, 6);
             cloudMeshLoader = () -> new MultiMesh(
                 new HexSkyMesh(this, 11, 0.15f, 0.13f, 5, new Color().set(Pal.spore).mul(0.9f).a(0.75f), 2, 0.45f, 0.9f, 0.38f),
