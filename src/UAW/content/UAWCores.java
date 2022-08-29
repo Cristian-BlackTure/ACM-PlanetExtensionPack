9121f49b32e6085d3d845ad3d58c60c380e5ac52
Cristian-BlackTure <110348555+Cristian-BlackTure@users.noreply.github.com>
2022-08-29 14:35:26 +0300

    Update UAWCores.java

--- src/UAW/content/UAWCores.java
+++ src/UAW/content/UAWCores.java
@@ -1,58 +1,87 @@
-package UAW.content; 
+package UAW.content;
 
-import arc.*; 
-import arc.graphics.*; 
-import arc.graphics.g2d.*; 
-import arc.struct.*; 
-import mindustry.content.*; 
-import mindustry.ctype.*; 
-import mindustry.entities.bullet.*; 
-import mindustry.gen.*; 
-import mindustry.graphics.*; 
-import mindustry.type.*; 
-import mindustry.world.*; 
-import mindustry.world.blocks.*; 
-import mindustry.world.blocks.campaign.*; 
-import mindustry.world.blocks.defense.*; 
-import mindustry.world.blocks.defense.turrets.*; 
-import mindustry.world.blocks.distribution.*; 
-import mindustry.world.blocks.environment.*; 
-import mindustry.world.blocks.experimental.*; 
-import mindustry.world.blocks.legacy.*; 
-import mindustry.world.blocks.liquid.*; 
-import mindustry.world.blocks.logic.*; 
-import mindustry.world.blocks.power.*; 
-import mindustry.world.blocks.production.*; 
-import mindustry.world.blocks.sandbox.*; 
-import mindustry.world.blocks.storage.*; 
-import mindustry.world.blocks.units.*; 
-import mindustry.world.consumers.*; 
-import mindustry.world.draw.*; 
-import mindustry.world.meta.*; 
- // End 
-import static mindustry.type.ItemStack.*; 
-  
- public class ACMBlocks implements ContentList{ 
-     public static Block 
-  
-     //cores
+import arc.Core;
+import arc.graphics.Color;
+import arc.graphics.g2d.Draw;
+import arc.graphics.g2d.Fill;
+import arc.graphics.g2d.Lines;
+import arc.math.Angles;
+import arc.math.Interp;
+import arc.math.Mathf;
+import arc.util.Time;
+import arc.util.Tmp;
+import mindustry.content.*;
+import mindustry.entities.Effect;
+import mindustry.entities.Lightning;
+import mindustry.entities.UnitSorts;
+import mindustry.entities.bullet.BasicBulletType;
+import mindustry.entities.bullet.ContinuousFlameBulletType;
+import mindustry.entities.part.DrawPart;
+import mindustry.entities.part.FlarePart;
+import mindustry.entities.part.RegionPart;
+import mindustry.entities.pattern.ShootBarrel;
+import mindustry.entities.pattern.ShootMulti;
+import mindustry.entities.pattern.ShootPattern;
+import mindustry.entities.pattern.ShootSpread;
+import mindustry.gen.Bullet;
+import mindustry.gen.Sounds;
+import mindustry.graphics.CacheLayer;
+import mindustry.graphics.Drawf;
+import mindustry.graphics.Layer;
+import mindustry.graphics.Pal;
+import mindustry.type.Category;
+import mindustry.type.ItemStack;
+import mindustry.type.LiquidStack;
+import mindustry.world.Block;
+import mindustry.world.blocks.defense.Door;
+import mindustry.world.blocks.defense.ForceProjector;
+import mindustry.world.blocks.defense.MendProjector;
+import mindustry.world.blocks.defense.Wall;
+import mindustry.world.blocks.defense.turrets.ItemTurret;
+import mindustry.world.blocks.defense.turrets.LaserTurret;
+import mindustry.world.blocks.defense.turrets.PointDefenseTurret;
+import mindustry.world.blocks.defense.turrets.PowerTurret;
+import mindustry.world.blocks.distribution.*;
+import mindustry.world.blocks.environment.Floor;
+import mindustry.world.blocks.environment.OreBlock;
+import mindustry.world.blocks.environment.StaticWall;
+import mindustry.world.blocks.liquid.Conduit;
+import mindustry.world.blocks.liquid.LiquidBridge;
+import mindustry.world.blocks.liquid.LiquidRouter;
+import mindustry.world.blocks.power.Battery;
+import mindustry.world.blocks.power.ConsumeGenerator;
+import mindustry.world.blocks.power.PowerNode;
+import mindustry.world.blocks.production.GenericCrafter;
+import mindustry.world.blocks.production.SolidPump;
+import mindustry.world.blocks.sandbox.PowerVoid;
+import mindustry.world.blocks.storage.StorageBlock;
+import mindustry.world.draw.*;
+import mindustry.world.meta.Attribute;
+import mindustry.world.meta.BuildVisibility;
+
+import static arc.graphics.g2d.Lines.lineAngle;
+import static mindustry.Vars.tilesize;
+import static mindustry.type.ItemStack.with;
+
+public class UAWCores{
+
+     //cores
       coreintel
        
-    @Override 
-     public void load(){ 
-         // region cores
+    @Override 
+    public void load(){ 
+      // region cores
  
-            coreintel = new coreBlock("core-intel"){{ 
-             requirements(Category.effect, with(Items.silicon, 4500)); 
-             health = 35000; 
+        coreintel = new coreBlock("core-intel"){{ 
+        requirements(Category.effect, with(Items.silicon, 4500)); 
+            health = 35000; 
              itemCapacity = 35000;
              unitCapModifier = 14;
-             size = 4; 
+             size = 4; 
              alwaysUnlocked = true;
              research = {
                  root = true
                  name = tyra
-                 }
-         }};
- }
        
+
+    
