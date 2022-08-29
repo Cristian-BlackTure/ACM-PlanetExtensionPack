e836e096d0509704e6a190bf77f88cf187734408
Cristian-BlackTure <110348555+Cristian-BlackTure@users.noreply.github.com>
2022-08-29 14:15:05 +0300

    Update UAWCores.java

--- src/UAW/content/UAWCores.java
+++ src/UAW/content/UAWCores.java
@@ -1,36 +1,36 @@
 package UAW.content; 
 
- import arc.*; 
- import arc.graphics.*; 
- import arc.graphics.g2d.*; 
- import arc.struct.*; 
- import mindustry.content.*; 
- import mindustry.ctype.*; 
- import mindustry.entities.bullet.*; 
- import mindustry.gen.*; 
- import mindustry.graphics.*; 
- import mindustry.type.*; 
- import mindustry.world.*; 
- import mindustry.world.blocks.*; 
- import mindustry.world.blocks.campaign.*; 
- import mindustry.world.blocks.defense.*; 
- import mindustry.world.blocks.defense.turrets.*; 
- import mindustry.world.blocks.distribution.*; 
- import mindustry.world.blocks.environment.*; 
- import mindustry.world.blocks.experimental.*; 
- import mindustry.world.blocks.legacy.*; 
- import mindustry.world.blocks.liquid.*; 
- import mindustry.world.blocks.logic.*; 
- import mindustry.world.blocks.power.*; 
- import mindustry.world.blocks.production.*; 
- import mindustry.world.blocks.sandbox.*; 
- import mindustry.world.blocks.storage.*; 
- import mindustry.world.blocks.units.*; 
- import mindustry.world.consumers.*; 
- import mindustry.world.draw.*; 
- import mindustry.world.meta.*; 
+import arc.*; 
+import arc.graphics.*; 
+import arc.graphics.g2d.*; 
+import arc.struct.*; 
+import mindustry.content.*; 
+import mindustry.ctype.*; 
+import mindustry.entities.bullet.*; 
+import mindustry.gen.*; 
+import mindustry.graphics.*; 
+import mindustry.type.*; 
+import mindustry.world.*; 
+import mindustry.world.blocks.*; 
+import mindustry.world.blocks.campaign.*; 
+import mindustry.world.blocks.defense.*; 
+import mindustry.world.blocks.defense.turrets.*; 
+import mindustry.world.blocks.distribution.*; 
+import mindustry.world.blocks.environment.*; 
+import mindustry.world.blocks.experimental.*; 
+import mindustry.world.blocks.legacy.*; 
+import mindustry.world.blocks.liquid.*; 
+import mindustry.world.blocks.logic.*; 
+import mindustry.world.blocks.power.*; 
+import mindustry.world.blocks.production.*; 
+import mindustry.world.blocks.sandbox.*; 
+import mindustry.world.blocks.storage.*; 
+import mindustry.world.blocks.units.*; 
+import mindustry.world.consumers.*; 
+import mindustry.world.draw.*; 
+import mindustry.world.meta.*; 
  // End 
- import static mindustry.type.ItemStack.*; 
+import static mindustry.type.ItemStack.*; 
   
  public class ACMBlocks implements ContentList{ 
      public static Block 
