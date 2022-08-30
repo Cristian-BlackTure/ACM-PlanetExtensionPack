b79919207fcb98c495e51cff7fd6bb6e1481235c
Cristian-BlackTure <110348555+Cristian-BlackTure@users.noreply.github.com>
2022-08-30 23:19:25 +0300

    Add files via upload

--- src/UAW/content/UAWItems.java
+++ src/UAW/content/UAWItems.java
@@ -1,30 +1,108 @@
 package UAW.content;
 
-import UAW.audiovisual.UAWPal;
-import arc.graphics.Color;
-import mindustry.type.Item;
+import arc.graphics.*;
+import arc.struct.*;
+import mindustry.type.*;
 
-public class UAWItems {
-	public static Item placeholder,
-                //vertal
-		cryogel, anthracite, compositeAlloy, dieselCore, mm;
 
-	public static void load() {
-		cryogel = new Item("item-cryogel", Color.valueOf("87ceeb")) {{
-			flammability = -10f;
-			explosiveness = 0f;
-		}};
-		anthracite = new Item("item-anthracite", Color.valueOf("272727")) {{
-			flammability = 1.8f;
-			explosiveness = 0.25f;
-			hardness = 4;
-		}};
-		compositeAlloy = new Item("item-composite-alloy", UAWPal.compAlloyMid) {{
-			cost = 2.5f;
-		}};
-		mm = new Item("item-mm", UAWPal.compAlloyMid) {{
-			cost = 2.5f;
-		}};
+public class ACMItems{
+    public static Item
+    scrap, copper, lead, graphite, coal, titanium, thorium, silicon, plastanium,
+    phaseFabric, surgeAlloy, sporePod, sand, blastCompound, pyratite, metaglass;
 
-	}
-}
+    //TODO remove, these are for debugging only
+    public static final Seq<Item> tyraItems = new Seq<>();
+    
+        public static void load(){
+        copper = new Item("copper", Color.valueOf("d99d73")){{
+            hardness = 1;
+            cost = 0.5f;
+            alwaysUnlocked = true;
+        }};
+
+        lead = new Item("lead", Color.valueOf("8c7fa9")){{
+            hardness = 1;
+            cost = 0.7f;
+            alwaysUnlocked = true;
+        }};
+
+        metaglass = new Item("metaglass", Color.valueOf("ebeef5")){{
+            cost = 1.5f;
+        }};
+
+        graphite = new Item("graphite", Color.valueOf("b2c6d2")){{
+            cost = 1f;
+        }};
+
+        sand = new Item("sand", Color.valueOf("f7cba4")){{
+            lowPriority = true;
+            alwaysUnlocked = true;
+            buildable = false;
+        }};
+
+        coal = new Item("coal", Color.valueOf("272727")){{
+            explosiveness = 0.2f;
+            flammability = 1f;
+            hardness = 2;
+            buildable = false;
+        }};
+
+        titanium = new Item("titanium", Color.valueOf("8da1e3")){{
+            hardness = 3;
+            cost = 1f;
+        }};
+
+        thorium = new Item("thorium", Color.valueOf("f9a3c7")){{
+            explosiveness = 0.2f;
+            hardness = 4;
+            radioactivity = 1f;
+            cost = 1.1f;
+            healthScaling = 0.2f;
+        }};
+
+        scrap = new Item("scrap", Color.valueOf("777777")){{
+
+        }};
+
+        silicon = new Item("silicon", Color.valueOf("53565c")){{
+            cost = 0.8f;
+        }};
+
+        plastanium = new Item("plastanium", Color.valueOf("cbd97f")){{
+            flammability = 0.1f;
+            explosiveness = 0.2f;
+            cost = 1.3f;
+            healthScaling = 0.1f;
+        }};
+
+        phaseFabric = new Item("phase-fabric", Color.valueOf("f4ba6e")){{
+            cost = 1.3f;
+            radioactivity = 0.6f;
+            healthScaling = 0.25f;
+        }};
+
+        surgeAlloy = new Item("surge-alloy", Color.valueOf("f3e979")){{
+            cost = 1.2f;
+            charge = 0.75f;
+            healthScaling = 0.25f;
+        }};
+
+        sporePod = new Item("spore-pod", Color.valueOf("7457ce")){{
+            flammability = 1.15f;
+            buildable = false;
+        }};
+
+        blastCompound = new Item("blast-compound", Color.valueOf("ff795e")){{
+            flammability = 0.4f;
+            explosiveness = 1.2f;
+            buildable = false;
+        }};
+
+        pyratite = new Item("pyratite", Color.valueOf("ffaa5f")){{
+            flammability = 1.4f;
+            explosiveness = 0.4f;
+            buildable = false;
+        }};
+        
+        }
+        }
