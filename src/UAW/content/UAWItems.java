9f064bcb71aebb29f3c7350f0092b9ae13462ec7
Cristian-BlackTure <110348555+Cristian-BlackTure@users.noreply.github.com>
2022-08-30 23:26:48 +0300

    Create UAWItems.java

---
+++ src/UAW/content/UAWItems.java
@@ -0,0 +1,26 @@
+package UAW.content;
+
+import UAW.audiovisual.UAWPal;
+import arc.graphics.Color;
+import mindustry.type.Item;
+
+public class UAWItems {
+	public static Item placeholder,
+		cryogel, anthracite, compositeAlloy, dieselCore;
+
+	public static void load() {
+		cryogel = new Item("item-cryogel", Color.valueOf("87ceeb")) {{
+			flammability = -10f;
+			explosiveness = 0f;
+		}};
+		anthracite = new Item("item-anthracite", Color.valueOf("272727")) {{
+			flammability = 1.8f;
+			explosiveness = 0.25f;
+			hardness = 4;
+		}};
+		compositeAlloy = new Item("item-composite-alloy", UAWPal.compAlloyMid) {{
+			cost = 2.5f;
+		}};
+
+	}
+}
