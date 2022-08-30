052e316535f5bda2c188192f10006b4f61740547
Cristian-BlackTure <110348555+Cristian-BlackTure@users.noreply.github.com>
2022-08-30 22:37:03 +0300

    Add files via upload

---
+++ src/UAW/content/TyraTechTree.java
@@ -0,0 +1,32 @@
+package mindustry.content;
+
+import arc.struct.*;
+import mindustry.game.Objectives.*;
+
+import static mindustry.content.Blocks.*;
+import static mindustry.content.SectorPresets.*;
+import static mindustry.content.SectorPresets.craters;
+import static mindustry.content.TechTree.*;
+import static mindustry.content.UnitTypes.*;
+import static UAW.content.UAWCores.*;
+import static UAW.content.ACMBlock.*;
+
+public class TyraTechTree{
+
+    public static void load(){
+        UAWPlanets.tyra.techTree = nodeRoot("tyra", coreIntel, () -> {
+
+            node(conveyor, () -> {
+
+                node(junction, () -> {
+                    node(router, () -> {
+                        node(launchPad, Seq.with(new SectorComplete(extractionOutpost)), () -> {
+                          
+                          });
+                          });
+                          });
+                          });
+                          });
+                          }
+                          }
+                        
